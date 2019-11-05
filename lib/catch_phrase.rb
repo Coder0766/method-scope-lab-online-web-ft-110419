6667a9dcb543978d3175725cfa1c6c344452892a
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-05 23:26:32 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -5,13 +5,3 @@ end
 
 catch_phrase
 
-
-# def catch_phrase(phrase)
-#   phrase = "It's-a me, Mario!"
-#   puts phrase
-# end
-
-# def catch_phrase(phrase = "It's-a me, Mario!")
-#   puts #{phrase}
-#   phrase = "It's-a me, Mario!"
-# end
